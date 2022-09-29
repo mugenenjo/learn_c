611bf10aa5dc5ee249e10ea4152a4e525eb93e2d
nattapol <nattapol.kritsuthikul@gmail.com>
2022-09-29 14:10:28 +0700

    first ommit

---
+++ assign2_1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Size = %lu\n", sizeof(size_t));
+    printf("Size of int = %ld\n", sizeof(int));
+    printf("Size of long = %ld\n", sizeof(long));
+    printf("Size of long long = %ld\n", sizeof(long long));
+    return 0;
+}
